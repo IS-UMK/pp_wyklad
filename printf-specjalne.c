cd5594c9d411a6d013d4175b1709eda335104042
Marek <grochu@is.umk.pl>
2025-10-20 11:36:11 +0200

    Modyfikacja printf-specjalnie

--- printf-specjalne.c
+++ printf-specjalne.c
@@ -11,8 +11,8 @@
 
 int main()
 {
-   printf("P\tj\tz\nr\te\ta\no\ts\tb\ng\tt\ta\nr\t \t");
-   printf("w\na\t \tn\nm\t \te\no\nw\na\nn\ni\ne\n");
+   printf("P\tj\tz%cr\te\ta\no\ts\tb\ng\tt\ta\nr\t ", 10);
+   printf("\tw\n%x\t \tn\nm\t \te\no\nw\na\nn\ni\ne\n", 10);
 
    return 0;
 }
